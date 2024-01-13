daf04623862c7be07fe965816ba95fac0aeeb84b
Stephan <stephan.elter@gmail.com>
2024-01-13 23:18:38 +0100

    Saving local state
    Nur der aktuelle lokale Stand mit einer erweiterten Datei gitignore

---
+++ Master.java
@@ -0,0 +1,29 @@
+ /**
+ * 
+ */
+//package net.punktuelles;
+
+/**
+ * 
+ */
+public class Master {
+
+    /**
+     * 
+     */
+    public Master() {
+        // TODO Auto-generated constructor stub
+        System.out.println("Im Konstruktor Master...");
+
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("Im Konstruktor Master...");
+    }
+
+}
